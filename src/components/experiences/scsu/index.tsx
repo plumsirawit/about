5d248276dcb0f5d7797cadc0d82ab76881e0d96d
Sirawit <plurm2545@hotmail.com>
2021-05-05 14:15:26 +0700

    fix: update layout to conform with others

--- src/components/experiences/scsu/index.tsx
+++ src/components/experiences/scsu/index.tsx
@@ -7,17 +7,19 @@ export const ExperienceSCSUPanel = () => {
     <PagePanel className="experience-scsu">
       <div className="overlay" />
       <div className="main">
-        <h1 className="header">
-          Instructor for Silpakorn University team to compete in Thailand
-          Olympiad in Informatics
-        </h1>
-        <p className="body">
-          I've teached a few groups of competitive programmers to help for their
-          preparations for the annual national programming competitions in
-          Thailand, Thailand's Olympiad in Informatics (TOI). I have a strong
-          passion in teaching new-generation students, fostering their
-          programming and problem solving skills.
-        </p>
+        <div className="description">
+          <h1 className="header">
+            Instructor for Silpakorn University team to compete in Thailand
+            Olympiad in Informatics
+          </h1>
+          <p className="body">
+            I've teached a few groups of competitive programmers to help for
+            their preparations for the annual national programming competitions
+            in Thailand, Thailand's Olympiad in Informatics (TOI). I have a
+            strong passion in teaching new-generation students, fostering their
+            programming and problem solving skills.
+          </p>
+        </div>
       </div>
     </PagePanel>
   );
